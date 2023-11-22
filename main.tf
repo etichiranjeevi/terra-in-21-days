d19fcd1b500fa0af72290b85055aed1522c41ef8
Chiranjeevi Eti <126656348+etichiranjeevi@users.noreply.github.com>
2023-11-22 13:37:51 +0530

    Add first ec2 instance (#2)

---
+++ main.tf
@@ -0,0 +1,11 @@
+resource "aws_instance" "app_server" {
+    ami = "ami-0d92749d46e71c34c"
+    instance_type = "t2.micro"
+
+    tags = {
+      Name = "ExampleAppServerInstance"
+      Owner = "Chiranjeevi"
+    }
+
+  
+}
